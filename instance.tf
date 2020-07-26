e1e642f0b174d8639785cc029f0bd9efebf801bb
Yasar ERKAN <ysr.erk2019@gmail.com>
2020-07-26 18:01:07 -0400

    new updated

--- instance.tf
+++ instance.tf
@@ -6,13 +6,28 @@ provider "aws" {
 resource "aws_instance" "terraexample" {
   ami           = "ami-2757f631"
   instance_type = "t2.micro"
-  //key_name = "codingbook"
+  key_name = "codysrKey"
   subnet_id = aws_subnet.public_subnet.id
   associate_public_ip_address = true
   tags={
       Name="web_server"
   }
 
-  //depends_on=[aws_vpc.main]
+  
+connection {
+    type = "ssh"
+    user = "ubuntu"
+    private_key = file("~/.ssh/id_rsa")
+    host = self.public_ip
+ }
+provisioner "remote-exec" {
+     inline = [
+        "sudo apt update",
+        "sudo apt install nginx -y",
+        "sudo systemctl start nginx",
+        "sudo systemctl enable nginx"
+   ]
+ }
+ 
 
 }
